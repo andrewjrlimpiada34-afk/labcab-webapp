16ae77c9436d71df88ae565674ec36bef200ac04
Severino <severino.perfumeshop@gmail.com>
2026-05-16 23:55:13 +0000

    Use the same Firebase config
    In your webapp, ensure src/firebase/config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,15 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBs3hImCWEk05cl8qudS2SLeF1O5_qP_oU",
+  authDomain: "thesis-labcab.firebaseapp.com",
+  projectId: "thesis-labcab",
+  storageBucket: "thesis-labcab.firebasestorage.app",
+  messagingSenderId: "274010692387",
+  appId: "1:274010692387:web:a4994620ac8bb3f609ca96",
+  measurementId: "G-FMHE2FTE1N"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
